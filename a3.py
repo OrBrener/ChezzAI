291d333c4d9e0834beca51d4c65747741e4a00b5
Or Brener <Or.Brener@gmail.com>
2025-03-04 03:55:16 -0500

    add: restore checkmate message functionality in main execution flow

--- a3.py
+++ a3.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
 from Chezz import *
-# import sys
 
 if __name__ == "__main__":
 
     game = Chezz()
     print(game)
 
-    # if game.is_checkmate():
-    #     print("It is the end of the game, you already won, good job! :)")
-    # else:
-    #     game.generate_board_files()
-    game.generate_board_files()
-            
+    if game.is_checkmate():
+        print("It is the end of the game, you already won, good job! :)")
+    else:
+        game.generate_board_files()            
