6db2cc1a36746fadd836abe7518c8e45d8236ac2
Or Brener <or.brener@gmail.com>
2025-04-18 00:27:49 -0400

    feat: AI move works! reverted move encoding to original + update fetchLegalMoves() in frontend. TODO: add back in move highlighting in frontend (removed by accident)

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def beg():
 @app.route('/legal_moves', methods=['GET'])
 def legal_moves():
     # Retrieve legal moves from the current game state.
-    
-    moves = [ str(m) for m in game.get_legal_moves() ]
-    return jsonify(moves)
+    moves = game.get_legal_moves()    # now a list of (piece,from,to)
+    # Convert each tuple to a list so jsonify can encode it
+    return jsonify([ [p, f, t] for p,f,t in moves ])
 
 @app.route('/next_move', methods=['POST'])
 def next_move():
@@ -27,8 +27,8 @@ def next_move():
     game.board = game.board.generate_board_after_move(user_move)
 
     # # 2) Generate the AI’s response
-    # best_move = game.max_score(game, 2)[1]
-    # game.board.generate_board_after_move(best_move)
+    best_move = game.max_score(game, 2)[1]
+    game.board = game.board.generate_board_after_move(best_move)
 
     # 3) Grab the new board state
     #    You can return either FEN or your position‐object
@@ -37,7 +37,7 @@ def next_move():
     # 4) Send back both moves and new position
     return jsonify({
         'user_move': user_move,
-        # 'ai_move':    ai_move,
+        'ai_move':    best_move,
         'updated_position': updated_position
 })
 
